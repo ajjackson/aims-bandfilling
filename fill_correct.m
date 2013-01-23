5d3e7e7c29a1e33491d984a3f976a067052d27d7
Adam Jackson <a.j.jackson@bath.ac.uk>
2013-01-23 14:10:09 +0000

    -Account for occupation in correction value -Remove redundant eigenvalue sum check -Add electronic occupation check -Clarify assumptions with comments

--- fill_correct.m
+++ fill_correct.m
@@ -27,29 +27,31 @@ kpoints(j)=importdata(sprintf('%s/kpoint-a%s',directory,name));
 j = j+1;
 end
 
-%% Calculate energy level contributions and sum
+% Using time reversal symmetry and assuming first k-point is gamma point
+k_weight(1,1) = 1;
+k_weight(2:length(kpoints),:) = 2;
 
-for i=1:length(kpoints)
-kpoints(i).contrib = kpoints(i).data(:,2).*kpoints(i).data(:,4);
-end
+%% Get eigenvalues and occupancies of lowest filled energy levels
 
-for i = 1:length(kpoints)
-kpoints(i).sum = sum(kpoints(i).contrib);
-end
-
-%% Get eigenvalues of lowest filled energy levels
+min_point = 1; % Assume that minimum is at gamma point
 
 for i = 1:length(kpoints)
-eigs(:,i) = kpoints(i).data(:,4);
+eigs(:,i,1) = kpoints(i).data(:,4);
+eigs(:,i,2) = kpoints(i).data(:,2);
 end
 
-eigs_trim=eigs(kpoints(1).data(:,2)~=0,:);
-low_eigs = eigs_trim(end,:);
+eigs_trim=eigs(kpoints(min_point).data(:,2)~=0,:,:);
+band(:,1) = eigs_trim(end,:,1)';
+occupancy(:,1) = eigs_trim(end,:,2)';
 
 %% Calculate band filling correction energy
 
-correction = min(low_eigs) - ...
-             (low_eigs(1) + 2*sum(low_eigs(2:end)))/(length(low_eigs)*2-1);
+contributions = (band-band(min_point)).*occupancy.*k_weight;
+    correction = sum(contributions)/sum(k_weight);
          
 fprintf('Band filling correction: %f eV\n', correction);
          
+%% Check occupancy is sane
+total_occupation = sum(occupancy.*k_weight)/sum(k_weight);
+
+fprintf('Conduction band occupation: %f electrons\n',total_occupation);
